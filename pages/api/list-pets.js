41a5e8ade8afd73400b6cc8cdd38d1e43e201eaf
Philipp Fiesel <Philipp.Fiesel@Student.Reutlingen-University.de>
2023-12-16 11:15:01 +0100

    Erstellung der list-pets.js, add-pet.js, post.js und umschreibung der hello.js

--- pages/api/list-pets.js
+++ pages/api/list-pets.js
@@ -2,9 +2,8 @@ import { sql } from '@vercel/postgres';
  
 export default async function handler(request, response) {
   try {
-    const result =
-      await sql`CREATE TABLE IF NOT EXISTS Pets ( Name varchar(255), Owner varchar(255) )`;
-    return response.status(200).json({ result });
+    const pets = await sql`SELECT * FROM Pets;`;
+    return response.status(200).json( pets.rows);
   } catch (error) {
     return response.status(500).json({ error });
   }
